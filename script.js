e6bf8079d8f00f2756301ebd4990fda4987b3e9b
DevDiscere <dev.ginomiguel@gmail.com>
2025-06-14 17:58:36 +0800

    Create main content, design, and logic of web app
    I created the structure and main content of the "Page by Page" library
    app with a big header for the name of the app and a book gallery showing
    the books you have read in a card format.
    
    The layout of the library app utilized CSS Grid and was designed with
    a color palette that matches the vibes of a book/library.
    
    For the logic of the web app, I wrote an object constructor for the
    books. Each book is an instance of this object that contain details such
    as the title, author, published date, genre, and a unique identifier
    generated using the crypto.randomUUID function. The book object is then
    stored in an array and is then displayed to the library app.
    
    For testing the display and logic, I added five books of my choosing
    using the addBookToLibrary function.

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-let paragraph = document.querySelector("p");
-paragraph.style.outline = "1px solid black";
+const myLibrary = [];
+
+// Object constructor for Books
+function Book(id, title, author, datePublished, genre) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.datePublished = datePublished;
+    this.genre = genre;
+}
+
+function addBookToLibrary(title, author, datePublished, genre) {
+    let book = new Book(crypto.randomUUID(), title, author, datePublished, genre);
+    myLibrary.push(book);
+}
+
+function displayToLibrary() {
+    const bookTitles = document.querySelectorAll(".title");
+    const bookAuthors = document.querySelectorAll(".author");
+    const bookDatePublished = document.querySelectorAll(".date-published");
+    const bookGenre = document.querySelectorAll(".genre");
+    let i = 0;
+
+    myLibrary.forEach(book => {
+        bookTitles[i].textContent = book.title;
+        bookAuthors[i].textContent = book.author;
+        bookDatePublished[i].textContent = book.datePublished;
+        bookGenre[i].textContent = book.genre;
+        i++;
+    });
+}
+
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", "July 11, 1960", "Southern Gothic, Bildungsroman");
+addBookToLibrary("Fahrenheit 451", "Ray Bradbury", "October 19, 1953", "Dystopian");
+addBookToLibrary("Pride and Prejudice", "Jane Austen", "January 28, 1813", "Classic Regency, Romance");
+addBookToLibrary("Odyssey", "Homer", "1614", "Epic");
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", "April 10, 1925", "Tragedy");
+
+displayToLibrary();
