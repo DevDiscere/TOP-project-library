4260c5294365292b0af09c38c3994805b328a12b
DevDiscere <dev.ginomiguel@gmail.com>
2025-06-16 11:44:03 +0800

    Add 'New Book' functionality with modal and form
    I added a button that allows the user to add a 'New Book' in their
    library. Clicking the button shows a modal containing a form that the
    user can fill the details of the new book they would like to add. Once
    the form is submitted, a new book card is displayed in their book
    gallery. I also added two new details for the book: Number of pages and
    Have they read the book?.
    
    As for the script, I removed the displayToLibrary function and replaced
    it with the createBookCard function. This function makes it so that the
    displayed book cards are created programatically and ensures that the
    code is highly maintainable. Additionally, the addBookToLibrary function
    also utilizes the createBookCard function.
    
    For the modal form, I added three eventListeners that would show the
    modal, close the modal, and submit the form inside the modal. When the
    form is submitted, it also clears the values inside of the input
    elements.

--- script.js
+++ script.js
@@ -1,39 +1,122 @@
 const myLibrary = [];
 
 // Object constructor for Books
-function Book(id, title, author, datePublished, genre) {
+function Book(id, title, author, numberOfPages, datePublished, genre, hasReadBook) {
     this.id = id;
     this.title = title;
     this.author = author;
+    this.numberOfPages = numberOfPages
     this.datePublished = datePublished;
     this.genre = genre;
+    this.hasReadBook = hasReadBook;
 }
 
-function addBookToLibrary(title, author, datePublished, genre) {
-    let book = new Book(crypto.randomUUID(), title, author, datePublished, genre);
+function createBookCard(title, author, numberOfPages, datePublished, genre, hasReadBook) {
+    const bookGallery = document.querySelector(".book-gallery");
+    const bookCard = document.createElement("article");
+    const bookTitle = document.createElement("h3");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    const bookDatePublished = document.createElement("p");
+    const bookGenre = document.createElement("p");
+    // Will change this into a pill
+    const bookHasReadBook = document.createElement("p");
+
+    bookCard.className = "book-card";
+    bookTitle.textContent = title;
+    bookAuthor.textContent = author;
+    bookPages.textContent = `${numberOfPages} pages`;
+    bookDatePublished.textContent = datePublished;
+    bookGenre.textContent = genre;
+    bookHasReadBook.textContent = hasReadBook ? "Yes, I have read the book" : "No, I haven't read the book";
+
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookDatePublished);
+    bookCard.appendChild(bookGenre);
+    bookCard.appendChild(bookHasReadBook);
+
+    bookGallery.appendChild(bookCard);
+}
+
+function addBookToLibrary(title, author, numberOfPages, datePublished, genre, hasReadBook) {
+    let book = new Book(crypto.randomUUID(), title, author, numberOfPages, datePublished, genre, hasReadBook);
+    
     myLibrary.push(book);
+
+    createBookCard(title, author, numberOfPages, datePublished, genre, hasReadBook);
 }
 
-function displayToLibrary() {
-    const bookTitles = document.querySelectorAll(".title");
-    const bookAuthors = document.querySelectorAll(".author");
-    const bookDatePublished = document.querySelectorAll(".date-published");
-    const bookGenre = document.querySelectorAll(".genre");
-    let i = 0;
+const newBookButton = document.querySelector(".new-book-button");
+const dialogModal = document.querySelector("dialog");
+const closeModalButton = document.querySelector(".close-modal-button");
+const modalForm = document.querySelector("form");
 
-    myLibrary.forEach(book => {
-        bookTitles[i].textContent = book.title;
-        bookAuthors[i].textContent = book.author;
-        bookDatePublished[i].textContent = book.datePublished;
-        bookGenre[i].textContent = book.genre;
-        i++;
-    });
-}
+newBookButton.addEventListener("click", () => {
+    dialogModal.showModal();
+});
 
-addBookToLibrary("To Kill a Mockingbird", "Harper Lee", "July 11, 1960", "Southern Gothic, Bildungsroman");
-addBookToLibrary("Fahrenheit 451", "Ray Bradbury", "October 19, 1953", "Dystopian");
-addBookToLibrary("Pride and Prejudice", "Jane Austen", "January 28, 1813", "Classic Regency, Romance");
-addBookToLibrary("Odyssey", "Homer", "1614", "Epic");
-addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", "April 10, 1925", "Tragedy");
+closeModalButton.addEventListener("click", () => {
+    dialogModal.close();
+});
 
-displayToLibrary();
+modalForm.addEventListener("submit", () => {
+    const modalFormData = new FormData(modalForm);
+    const modalFormInputs = modalForm.querySelectorAll("input");
+    
+    addBookToLibrary(
+        modalFormData.get("title"),
+        modalFormData.get("author"),
+        modalFormData.get("pages"),
+        modalFormData.get("date-published"),
+        modalFormData.get("genre"),
+        modalFormData.get("has-read-book")
+    );
+
+    // Clear form input values
+    for (const input of modalFormInputs) {
+        input.value = null;
+    }
+});
+
+addBookToLibrary(
+    "To Kill a Mockingbird", 
+    "Harper Lee", 
+    281, 
+    "July 11, 1960", 
+    "Southern Gothic, Bildungsroman", 
+    true
+);
+addBookToLibrary(
+    "Fahrenheit 451", 
+    "Ray Bradbury", 
+    156,
+    "October 19, 1953",
+    "Dystopian",
+    true
+);
+addBookToLibrary(
+    "Pride and Prejudice", 
+    "Jane Austen",
+    279, 
+    "January 28, 1813", 
+    "Classic Regency, Romance",
+    false
+);
+addBookToLibrary(
+    "Odyssey", 
+    "Homer",
+    541, 
+    "1614", 
+    "Epic",
+    true
+);
+addBookToLibrary(
+    "The Great Gatsby", 
+    "F. Scott Fitzgerald", 
+    180,
+    "April 10, 1925", 
+    "Tragedy",
+    false
+);
